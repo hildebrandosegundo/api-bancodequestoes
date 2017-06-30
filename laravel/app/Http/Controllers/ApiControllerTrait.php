5eec8cb1fff35cc56cc6387a7883d6938e184626
Hildebrando Alves de Araújo Segundo <hildebrandosegundo@gmail.com>
2017-06-30 09:11:46 -0300

    alteracoes na tabela de questoes e provas e adicao de login

--- laravel/app/Http/Controllers/ApiControllerTrait.php
+++ laravel/app/Http/Controllers/ApiControllerTrait.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 trait ApiControllerTrait
@@ -15,23 +16,48 @@ trait ApiControllerTrait
         $limit = $request->all()['limit'] ?? 15;
         $order = $request->all()['order'] ?? null;
 
-        if ($order!==null){
-            $order = explode(',',$order);
+        if ($order !== null) {
+            $order = explode(',', $order);
         }
         $order[0] = $order[0] ?? 'id';
         $order[1] = $order[1] ?? 'asc';
 
         $where = $request->all()['where'] ?? [];
         $like = $request->all()['like'] ?? null;
+        $likequestoes = $request->all()['likequestoes'] ?? null;
 
-        if($like){
-            $like = explode(',',$like);
-            $like[1] = '%'.$like[1].'%';
+        if ($like) {
+            $like = explode(',', $like);
+            $like[1] = '%' . $like[1] . '%';
         }
-        $result = $this->model->orderby($order[0],$order[1])
-            ->where(function ($query) use ($like){
+        $result = $this->model->orderby($order[0], $order[1])
+            ->where(function ($query) use ($like) {
                 if ($like) {
                     return $query->where($like[0], 'like', $like[1]);
+                }
+                return $query;
+            })
+            ->where(function ($query) use ($likequestoes) {
+                if ($likequestoes) {
+                    return $query->whereExists(function ($query) use ($likequestoes) {
+                        $query->select($query->raw('*'))
+                            ->from('areas')
+                            ->orwhereRaw('pquestoes.area_id = areas.id')
+                            ->orwhere('areas.area', 'like', '%' . $likequestoes . '%')
+                            /*->from('categorias')
+                            ->orwhereRaw('pquestoes.categoria_id = categorias.id')
+                            ->orwhere('series.serie', 'like', '%' . $likequestoes . '%')
+                            ->from('nivels')
+                            ->orwhereRaw('pquestoes.nivel_id = nivels.id')
+                            ->orwhere('categorias.categoria', 'like', '%' . $likequestoes . '%')
+                            ->from('series')
+                            ->orwhereRaw('pquestoes.serie_id = series.id')
+                            ->orwhere('habilidades.habilidade', 'like', '%' . $likequestoes . '%')
+                            ->from('habilidades')
+                            ->orwhereRaw('pquestoes.habilidade_id = habilidades.id')
+                            ->orwhere('nivels.nivel', 'like', '%' . $likequestoes . '%')*/;
+                    });
+
                 }
                 return $query;
             })
@@ -54,7 +80,7 @@ trait ApiControllerTrait
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -66,7 +92,7 @@ trait ApiControllerTrait
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -79,7 +105,7 @@ trait ApiControllerTrait
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -90,8 +116,8 @@ trait ApiControllerTrait
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -105,7 +131,7 @@ trait ApiControllerTrait
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
@@ -115,9 +141,10 @@ trait ApiControllerTrait
         $result->delete();
         return response()->json($result);
     }
+
     protected function relationships()
     {
-        if (isset($this->reletionships)){
+        if (isset($this->reletionships)) {
             return $this->reletionships;
         }
         return [];
